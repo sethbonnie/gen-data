a8e89226ae8ade4e9725ee899b93d812e7c7eee5
Seth Bonnie <sethkbonnie@gmail.com>
2016-09-18 18:05:58 -0500

    Move gen.js to top level (#13)
    There is no reason to have index.js just import then export gen.js.
    It only made sense to do that before when I was combining several
    submodules into one export. But that level of indirection is unnecessary.

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-module.exports = require('./src/gen')
+const gen = (spec) => {
+  if (typeof spec === 'number' ||
+      typeof spec === 'string' ||
+      typeof spec === 'undefined' ||
+      typeof spec === 'boolean' ||
+      spec === null)
+    return spec
 
+  if (typeof spec === 'function')
+    return spec()
+
+  if (spec instanceof Array) {
+    return spec.map((s) => gen(s))
+  }
+
+  let result = {}
+
+  for (let k in spec) {
+    result[k] = gen(spec[k])
+  }
+  return result
+}
+
+gen.address = require('./src/address')
+gen.random = require('./src/random')
+
+module.exports = gen
